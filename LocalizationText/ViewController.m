2485e87c47a0b9739f69ee4dd8207be1ffe06886
Peter Pan <PeterPan@Peters-MacBook-Pro.local>
2013-05-10 20:49:39 +0800

    first commit

--- LocalizationText/ViewController.m
+++ LocalizationText/ViewController.m
@@ -18,6 +18,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel *musicLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 50)];
+    musicLabel.text = NSLocalizedString(@"music", nil);
+    [self.view addSubview:musicLabel];
+    musicLabel.textAlignment = NSTextAlignmentCenter;
+    musicLabel.font = [UIFont systemFontOfSize:30];
+    
+    UILabel *bookLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 70, 300, 50)];
+    bookLabel.text = NSLocalizedString(@"book", nil);
+    [self.view addSubview:bookLabel];
+    bookLabel.textAlignment = NSTextAlignmentCenter;
+    bookLabel.font = [UIFont systemFontOfSize:30];
+    
 }
 
 - (void)didReceiveMemoryWarning
